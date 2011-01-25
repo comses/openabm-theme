b61cc3651a641c2f21185f72ce08a5dc23eb9f50
Nathan Rollins <nathan.rollins@asu.edu>
2011-01-25 17:12:55 +0000

    Clearing repository in order to import latest revision

--- tpl/block.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id$
-?>
-<!-- Block.tpl.php -->
- <div class="block block-<?php print $block->module; ?>" id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
-    <h2 class="title"><?php print $block->subject; ?></h2>
-    <div class="content"><?php print $block->content; ?></div>
- </div>
- <!-- End Block.tpl.php -->
