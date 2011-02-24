c9ec7eefeaf53bf2be3dca0b9215e27ff7dbe9a2
Nathan Rollins <nathan.rollins@asu.edu>
2011-02-24 23:31:26 +0000

    Deleted 1.x release for installation of 2.x

--- tpl/block-logintoboggan.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-// $Id: block.tpl.php 3 2010-04-02 22:57:10Z tsilva $
-?>
-<!-- block-logintoboggan.tpl.php -->
-<div class="block userInfo"><?php print $block->content; ?></div>
-<!-- End block-logintoboggan.tpl.php -->
