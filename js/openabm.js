8f3b72eebdec8d1edb73e191e05a7c3b04dcdfab
Nathan Rollins <nathan.rollins@asu.edu>
2013-04-16 22:54:23 -0700

    Multiple theme updates. CSS styling updates, including for comments, editor window, and ratings. Template overrides added for comments, ratings, and the node add page.

--- js/openabm.js
+++ js/openabm.js
@@ -1,34 +1,31 @@
-// $Id$
-$(document).ready(function() {
-  $("#striped tbody tr").mouseover(function() {
-    var row = $(this).attr("class").substring(0, 1);
-    $("#striped tbody tr." + row).addClass("highlight");
+(function ($) {
+  $(document).ready(function() {
+    $("#striped tbody tr").mouseover(function() {
+      var row = $(this).attr("class").substring(0, 1);
+      $("#striped tbody tr." + row).addClass("highlight");
+    });
 
+    $("#striped tbody tr").mouseout(function() {
+      var row = $(this).attr("class").substring(0, 1);
+      $("#striped tbody tr." + row).removeClass("highlight");
+    });	
+
+    // Load dialog on click
+    $('#basic-modal .basic').click(function (e) {
+      $('#basic-modal-content').modal();
+      return false;
+    });
   });
 
-  $("#striped tbody tr").mouseout(function() {
-    var row = $(this).attr("class").substring(0, 1);
-    $("#striped tbody tr." + row).removeClass("highlight");
-  });	
+  window.onload=function(){
+    Nifty("ul.button a", "transparent");
+    Nifty("a.button", "transparent");
+    Nifty("div.modelstatus", "transparent"); 
+    Nifty("div.versions-list", "transparent");
+  }
 
-  // Load dialog on click
-  $('#basic-modal .basic').click(function (e) {
-    $('#basic-modal-content').modal();
-
-    return false;
-  });
-
-});
-
-window.onload=function(){
-  Nifty("ul.button a", "transparent");
-  Nifty("a.button", "transparent");
-  Nifty("div.modelstatus", "transparent"); 
-  Nifty("div.versions-list", "transparent");
-}
-
-function DoNav(url)
-{
-  document.location.href = url;
-}
-
+  function DoNav(url)
+  {
+    document.location.href = url;
+  }
+})(jQuery);
